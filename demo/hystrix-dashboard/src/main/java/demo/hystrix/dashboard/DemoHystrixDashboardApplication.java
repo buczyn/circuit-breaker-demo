e3e20a74410cd1f778b6da958df8655005a7ac0e
Jacek Bukowski <buczyn@gmail.com>
2017-09-28 23:17:22 +0200

    Adding hystrix-dashboard.
    Configuring the hystrix dashboard.

--- demo/hystrix-dashboard/src/main/java/demo/hystrix/dashboard/DemoHystrixDashboardApplication.java
+++ demo/hystrix-dashboard/src/main/java/demo/hystrix/dashboard/DemoHystrixDashboardApplication.java
@@ -3,9 +3,11 @@ package demo.hystrix.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class DemoHystrixDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoHystrixDashboardApplication.class, args);
